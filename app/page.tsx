c39c21960a3432dd3ed757e64ac7f0f592ea5171
Ricardo <kadus.developer@gmail.com>
2025-11-30 23:44:19 -0300

    feat: add Header in landing-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
+import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
-      {
-        <Hero />
-        /*<Header />
+      <Header />
       <Hero />
-      <VideoExplanation />
+
+      {/* <VideoExplanation />
       <Pricing />
-      <FAQ />*/
-      }
+      <FAQ /> 
+      */}
     </div>
   );
 }
