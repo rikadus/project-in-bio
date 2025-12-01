b87cd680d9f84f078224ffead9195f8b7b145c9f
Ricardo <kadus.developer@gmail.com>
2025-12-01 01:29:14 -0300

    feat: implement Pricing section layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
+import VideoExplanation from "./components/landing-page/video-explanation";
+import Pricing from "./components/landing-page/pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
+      <VideoExplanation />
+      <Pricing />
 
       {/* <VideoExplanation />
       <Pricing />
