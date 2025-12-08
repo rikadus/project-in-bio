67d2eb6845aca2a158f117c9d93f3d799f1d952a
Ricardo <kadus.developer@gmail.com>
2025-12-08 19:53:28 -0300

    feat: Implement Stripe integration for user upgrades and subscription management with new API routes, hooks, and UI components.

---
+++ app/(pages)/[profileId]/upgrade/plan-buttons.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Button from "@/app/components/landing-page/ui/button";
+import { useStripe } from "@/app/hooks/useStripe";
+import { useParams } from "next/navigation";
+
+export default function PlanButtons() {
+const {createStripeCheckout} = useStripe();
+const {profileId} = useParams();
+    
+    return (
+        <div className="flex gap-4">
+                <Button onClick={() => 
+                  createStripeCheckout({
+                    isSubscription: true,
+                    metadata: {profileId}
+                  })}>R$ 9,90 / mês</Button>
+                {/* <Button onClick={() => createStripeCheckout('yearly')}>R$ 99,90 Vitalício</Button> */}
+              </div>
+    );
+}
--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     // @ts-ignore
-    apiVersion: "2025-11-17",
+    apiVersion: '2024-06-20',
     
 });
 
